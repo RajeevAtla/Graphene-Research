a7caff1113ce795c99562998d687b462e4653c6a
Rajeev Atla <rajeev@rajeevatla.com>
2020-07-18 00:31:27 -0400

    Add data_analyze_prelim.py and update data_import.py

---
+++ data_analyze_prelim.py
@@ -0,0 +1,8 @@
+#Import pandas and numpy
+import pandas as pd
+import numpy as np
+
+#Import the data_import.py file in this repo
+import data_import
+
+#Organize all the columns
---
+++ data_import.py
@@ -0,0 +1,8 @@
+#Import pandas, which is python's data analysis library
+import pandas as pd
+
+#load data from data file - 'dataset.csv'
+data = pd.read_csv("dataset.csv")
+
+#Sanity check - print the first 5 rows
+#print(data.head())
