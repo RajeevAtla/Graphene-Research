9a407c3ac8a190af98a052615e56ea2de6885527
Rajeev Atla <rajeev@rajeevatla.com>
2020-07-24 00:55:27 -0400

    Prototype of model

--- train.py
+++ train.py
@@ -1,9 +1,26 @@
 import pandas as pd
 import numpy as np
-import xgboost
 
-import sklearn.model_selection
+import xgboost as xgb
+from xgboost import DMatrix
+
+import sklearn
 from sklearn.model_selection import train_test_split
 
 import data_import
-exec(open('data_import.py').read())
+exec(open('data_analyze_prelim.py').read())
+
+train_data, test_data = train_test_split(data)
+
+dtrain = DMatrix(train_data, train_data["Superconductivity Type"])
+dtest = DMatrix(test_data, test_data["Superconductivity Type"])
+
+param = {'max_depth': 10, 'eta': 0.1, 'objective': 'reg:squarederror'}
+
+param['eval_metric'] = 'error'
+
+epochs = 100
+
+evallist = [(dtrain, 'eval'), (dtest, 'train')]
+
+bst = xgb.train(param, dtrain, epochs, evallist)
