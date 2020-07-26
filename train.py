64d1341e873962e2079a019a0fac48b2db0e7bc9
Rajeev Atla <rajeev@rajeevatla.com>
2020-07-26 00:27:49 -0400

    Change test:train ratio

--- train.py
+++ train.py
@@ -5,20 +5,28 @@ from sklearn.metrics import accuracy_score
 
 dataset = loadtxt('dataset_nolabel.csv', delimiter = ",", skiprows = 1) #loads the dataset, skips the labels
 
-X = dataset[:, 1:9] #input features
+X = dataset[:, 0:8] #input features
 Y = dataset[:, 10] #output label - type of superconductivity
 
+#Sanity check
+#print(X)
+#print(Y)
+
 seed = 101 #used to provide a degree of reproducibility
-test_size = 0.33 #train:test ratio
+test_size = 0.25 #train:test ratio
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = test_size, random_state = seed)
 
 model = XGBClassifier()
 model.fit(X_train, y_train)
+
+
 
 print(model)
 
 y_pred = model.predict(X_test)
 predictions = [round(value) for value in y_pred]
 
+#print(predictions) #sanitycheck
+
 accuracy = accuracy_score(y_test, predictions)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
+print(accuracy * 100)
