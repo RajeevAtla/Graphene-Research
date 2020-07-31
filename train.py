f7ed28576d8e1863ddc9b82fe85397bad6edffe4
Rajeev Atla <rajeev@rajeevatla.com>
2020-07-31 16:46:19 -0400

    Update

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 from numpy import loadtxt
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+import matplotlib.pyplot as plt
 
 dataset = loadtxt('dataset_nolabel.csv', delimiter = ",", skiprows = 1) #loads the dataset, skips the labels
 
@@ -14,13 +16,13 @@ Y = dataset[:, 10] #output label - type of superconductivity
 
 seed = 12 #used to provide a degree of reproducibility
 
-test_size = 0.25 #train:test ratio
+test_size = 0.2 #train:test ratio
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = test_size, random_state = seed)
 
 model = XGBClassifier()
 model.fit(X_train, y_train)
 
-#print(model)
+print(model)
 
 y_pred = model.predict(X_test)
 predictions = [round(value) for value in y_pred]
@@ -28,4 +30,4 @@ predictions = [round(value) for value in y_pred]
 #print(predictions) #sanitycheck
 
 accuracy = accuracy_score(y_test, predictions)
-print(accuracy * 100)
+print("Accuracy: ", accuracy * 100)
